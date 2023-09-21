3f5791906d0466100e2eb58f5072aa541632e199
Pussmal <aleksey.motorin2015@yandex.ru>
2023-09-21 14:14:57 +0300

    Initial Commit

---
+++ ProjectGit/ProjectGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGitApp.swift
+//  ProjectGit
+//
+//  Created by Алексей Моторин on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
